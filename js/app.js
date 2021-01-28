33bbf5a29790dbe0deca73f9a01ae1a5f918d7ef
许文彬 <programmerxwb.163.com>
2021-01-28 21:45:59 +0800

    begin

---
+++ js/app.js
@@ -0,0 +1,39 @@
+/* (async function() {
+	let mediaStream = await navigator.mediaDevices.getUserMedia({
+		video: false,
+		audio: true
+	});
+	document.querySelector("audio").srcObject = mediaStream;
+})();
+ */
+
+new Vue({
+	el: "#vueapp",
+	data: {
+		audoinputvices: [],
+		audioSelectedIndex: 0
+	},
+	mounted() {
+		this.initvueapp();
+	},
+
+	methods: {
+		async initvueapp() {
+			let devices = await navigator.mediaDevices.enumerateDevices();
+			let audioinputevices = devices.filter(value => value.kind === "audioinput");
+			this.audoinputvices.length = 0;
+			this.audoinputvices.push(...audioinputevices);
+			this.showSelectedDevices();
+		},
+		async showSelectedDevices(){
+			let deviceInfo = this.audoinputvices[this.audioSelectedIndex];
+			let stream = await navigator.mediaDevices.getUserMedia({vedis:false,audio:true});
+			this.$refs.audio.srcObject = stream;
+		}
+	},
+	watch:{
+		audioSelectedIndex(val,odlVal){
+			console.log(val);
+		}
+	}
+});
---
+++ js/vedio.js
@@ -0,0 +1,25 @@
+/* (async function() {
+	document.querySelector("video").srcObject = await navigator.mediaDevices.getUserMedia({
+		video: true,
+		audio: false
+	});
+})(); */
+new Vue({
+	el: "#vueapp",
+	mounted() {
+		this.initVueApp();
+	},
+	methods: {
+		takePhoto() {
+			this._context2d.drawImage(this.$refs.video, 0, 0, 400, 300);
+		},
+		async initVueApp() {
+			this.$refs.video.srcObject = await navigator.mediaDevices.getUserMedia({
+				video: true,
+				audio: false
+			});
+
+			this._context2d = this.$refs.canvas.getContext("2d");
+		}
+	}
+});
